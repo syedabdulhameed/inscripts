9c9c444ca84b8c784a384a440058f531184a1c66
Syed Abdul Hameed <105663298+syedabdulhameed@users.noreply.github.com>
2026-03-23 11:36:14 +0530

    Update ELKStack.sh

--- ELKStack.sh
+++ ELKStack.sh
@@ -1,1 +1,1 @@
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg && sudo apt-get install apt-transport-https && echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/9.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-9.x.list && sudo apt-get update && sudo apt-get install elasticsearch kibana logstash
+wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.10.0-amd64.deb
